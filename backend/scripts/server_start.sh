41ccbb9889b0e6ab8fdaa6ed9e087fe50e851660
jujang <jeongookjang@naver.com>
2022-12-04 12:10:55 +0900

    infra: 배포자동화 스크립트 - EC2 build 경로 수정

--- backend/scripts/server_start.sh
+++ backend/scripts/server_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /home/ubuntu/build
-sudo nohup java -jar build/libs/backend-0.0.1-SNAPSHOT.jar > logging.log 2>&1 &
+sudo nohup java -jar backend-0.0.1-SNAPSHOT.jar > logging.log 2>&1 &
